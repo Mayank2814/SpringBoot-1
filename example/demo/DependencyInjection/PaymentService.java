c1b3201f94208ffde2440a056c06dd136622b307
Mayank2814 <mayanksagar6458@gmail.com>
2026-07-13 14:32:55 +0530

    DependencyInjection

---
+++ example/demo/DependencyInjection/PaymentService.java
@@ -0,0 +1,10 @@
+package com.example.demo.DependencyInjection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+    public void payment(){
+        System.out.println("payment done");
+    }
+}
