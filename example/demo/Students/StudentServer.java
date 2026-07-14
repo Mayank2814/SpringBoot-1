da6982f465c5b2e4e2e5410f95bdc6e7b263dff4
Mayank2814 <mayanksagar6458@gmail.com>
2026-07-14 12:57:29 +0530

    Post

---
+++ example/demo/Students/StudentServer.java
@@ -0,0 +1,27 @@
+package com.example.demo.Students;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentServer {
+   // @GetMapping
+    @PostMapping("/create")
+    public String storeStudent(@RequestBody Student student){
+
+        int id=student.getId();
+        String name=student.getName();
+        int age=student.getAge();
+
+
+        return """
+                id:1
+                name : Mayank
+                Department : CSE
+                age: 21
+                """;
+    }
+
+}
